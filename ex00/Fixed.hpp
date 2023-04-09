9d44f5ae40f7177a45e867bc762d874d42cdec3e
Arik Hativa <arikhativa@gmail.com>
2023-04-09 16:59:03 +0300

    ex00

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+
+class Fixed
+{
+
+	public:
+
+		Fixed();
+		Fixed( Fixed const & src );
+		~Fixed();
+
+		Fixed &		operator=( Fixed const & rhs );
+		int			getRawBits(void) const;
+		void		setRawBits(int const raw);
+
+	private:
+		int					_value;
+		static const int	_fractional_bits = 8;
+
+};
+
+#endif /* *********************************************************** FIXED_H */

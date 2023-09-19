46b2909f2fdaeebdfbad27292fe1cf8416c09a1e
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2023-09-19 16:39:54 +0200

    added more tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,14 +6,39 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 15:36:12 by yrabby            #+#    #+#             */
-/*   Updated: 2023/09/19 15:36:12 by yrabby           ###   ########.fr       */
+/*   Updated: 2023/09/19 16:21:26 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 # include "Fixed.hpp"
 
-int main( void ) {
+
+static void test_setRawBits(void)
+{
+	Fixed a;
+
+	a.setRawBits(15 << 8);
+
+	std::cout << "a is " << a << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+}
+
+static void test_toFloat(void)
+{
+	Fixed a;
+
+	a.setRawBits(15 << 8);
+
+	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+
+	a = Fixed(-19.999f);
+	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+}
+
+static void main_test(void)
+{
 	Fixed a;
 	Fixed const b( 10 );
 	Fixed const c( 42.42f );
@@ -30,6 +55,13 @@ int main( void ) {
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+}
+
+int main( void )
+{
+	main_test();
+	test_setRawBits();
+	test_toFloat();
 
 	return 0;
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,14 +6,96 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 15:36:21 by yrabby            #+#    #+#             */
-/*   Updated: 2023/09/19 15:36:21 by yrabby           ###   ########.fr       */
+/*   Updated: 2023/09/19 16:39:29 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 # include "Fixed.hpp"
 
-int main( void ) {
+void test_min_max(void)
+{
+	std::cout << std::endl << " -- Tests test_min_max --" << std::endl << std::endl;
+	Fixed a(15);
+	Fixed b(2.5f);
+	
+	std::cout << "a: 15, b: 2.5" << std::endl;
+	std::cout << "expect 2.5\t->\t" << Fixed::min(a, b) << std::endl;
+	std::cout << "expect 15\t->\t" << Fixed::max(a, b) << std::endl;
+
+	const Fixed c(99.9f);
+	const Fixed d(99.8f);
+	std::cout << "c: 99.9, d: 99.8" << std::endl;
+	std::cout << "expect 99.8\t->\t" << Fixed::min(c, d) << std::endl;
+	std::cout << "expect 99.9\t->\t" << Fixed::max(c, d) << std::endl;
+	std::cout << std::endl << " -- Tests test_min_max -- (END)" << std::endl;
+}
+
+void test_compare(void)
+{
+	std::cout << std::endl << " -- Tests test_compare --" << std::endl << std::endl;
+	Fixed a(15);
+	Fixed b(2.5f);
+	
+	std::cout << "a: 15, b: 2.5" << std::endl;
+	std::cout << "expect 1\t->\t" << (a != b) << std::endl;
+	std::cout << "expect 0\t->\t" << (a == b) << std::endl;
+	std::cout << "expect 1\t->\t" << (a > b) << std::endl;
+	std::cout << "expect 0\t->\t" << (a < b) << std::endl;
+	std::cout << "expect 0\t->\t" << (a <= b) << std::endl;
+	std::cout << "expect 1\t->\t" << (a >= b) << std::endl;
+
+	b = Fixed(14.9999999999f);
+	std::cout << "a: 15, b: 14.9999999999" << std::endl;
+	std::cout << "expect 0\t->\t" << (a != b) << std::endl;
+	std::cout << "expect 1\t->\t" << (a == b) << std::endl;
+	std::cout << "expect 0\t->\t" << (a > b) << std::endl;
+	std::cout << "expect 0\t->\t" << (a < b) << std::endl;
+	
+	std::cout << "expect 1\t->\t" << (a <= b) << std::endl;
+	std::cout << "expect 1\t->\t" << (a >= b) << std::endl;
+	std::cout << std::endl << " -- Tests test_compare -- (END)" << std::endl;
+}
+
+void test_basic_math(void)
+{
+	std::cout << std::endl << " -- Tests test_basic_math --" << std::endl << std::endl;
+	Fixed a(15);
+	Fixed b(2.5f);
+	
+	std::cout << "expect 17.5\t->\t" << a + b << std::endl;
+	std::cout << "expect 12.5\t->\t" << a - b << std::endl;
+	std::cout << "expect 6\t->\t" << a / b << std::endl;
+	std::cout << "expect 37.5\t->\t" << a * b << std::endl;
+
+	a = b;
+	
+	std::cout << "expect 5\t->\t" << a + b << std::endl;
+	std::cout << "expect 0\t->\t" << a - b << std::endl;
+	std::cout << "expect 1\t->\t" << a / b << std::endl;
+	std::cout << "expect 6.25\t->\t" << a * b << std::endl;
+	std::cout << std::endl << " -- Tests test_basic_math -- (END)" << std::endl;
+}
+
+void test_inc_dec(void)
+{
+	std::cout << std::endl << " -- Tests test_inc_dec --" << std::endl << std::endl;
+	Fixed a;
+	
+	std::cout << "a should be 0: " << a++ << std::endl;
+	std::cout << "a should be 0.00390625: " << a << std::endl;
+
+	std::cout << "a should be 0.00390625: " << a-- << std::endl;
+	std::cout << "a should be 0: " << a << std::endl;
+
+	std::cout << "a should be -0.00390625: " << --a << std::endl;
+	std::cout << "a should be 0: " << ++a << std::endl;
+	std::cout << std::endl << " -- Tests test_inc_dec -- (END)" << std::endl;
+}
+
+void test_subject(void)
+{
+	std::cout << std::endl << " -- Tests From Subject --" << std::endl << std::endl;
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	
@@ -26,6 +108,17 @@ int main( void ) {
 	std::cout << b << std::endl;
 	
 	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << std::endl << " -- Tests From Subject -- (END)" << std::endl;
+
+}
+
+int main( void )
+{
+	test_subject();
+	test_inc_dec();
+	test_basic_math();
+	test_compare();
+	test_min_max();
 
 	return 0;
 }

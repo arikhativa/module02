b2ae6d1f5474478831df4ca4bd0fb90510a9bba8
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2023-09-19 16:14:31 +0200

    ex00 mofre tests

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,14 +6,35 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 15:36:03 by yrabby            #+#    #+#             */
-/*   Updated: 2023/09/19 15:36:04 by yrabby           ###   ########.fr       */
+/*   Updated: 2023/09/19 16:13:48 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 # include "Fixed.hpp"
 
-int main( void ) {
+static void setRawBits(void)
+{
+	Fixed a;
+
+	a.setRawBits(15);
+	Fixed b( a );
+	Fixed c;
+	
+	c = b;
+	
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	
+	c.setRawBits(11);
+	std::cout << c.getRawBits() << std::endl;
+	b = c;
+	std::cout << b.getRawBits() << std::endl;
+}
+
+static void main_test(void)
+{
 	Fixed a;
 	Fixed b( a );
 	Fixed c;
@@ -21,5 +42,11 @@ int main( void ) {
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+}
+
+int main( void )
+{
+	main_test();
+	setRawBits();
 	return 0;
 }
